bd3fbd2bd43c1906689aa01a00ad57ded35741b6
keshav-12345 <keshavbhatheja998@gmail.com>
2024-08-05 10:16:41 +0530

    done until Bottom Fragment

---
+++ app/src/main/java/com/example/admin_blinkit_clone/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.admin_blinkit_clone
+
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+
+class HomeFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setStatusBarColor()
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    private fun setStatusBarColor(){
+        activity?.window?.apply {
+            val statusBarColors = ContextCompat.getColor(requireContext(),R.color.yellow)
+            statusBarColor = statusBarColors
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.M){
+                decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            }
+        }
+    }
+}
